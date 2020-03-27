9d9acecc497cc005a114f7c6ce7bcbbf56c8ba98
Demola <ademoladebayo@gmail.com>
2020-03-27 19:41:21 +0000

    GeoJSON Location Geocoder hook Added

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 const crypto = require('crypto');
 //https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
-//
+
+const geocoder = require('../util/geocoder');
 
 //user schema
 const BootcampSchema = new mongoose.Schema({
@@ -52,7 +53,8 @@ const BootcampSchema = new mongoose.Schema({
     formattedAddress: String,
     street: String,
     city: String,
-    postcode: String,
+    state: String,
+    zipcode: String,
     country: String
   },
   careers: {
@@ -103,4 +105,26 @@ BootcampSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+
+  //Do not save address in DB
+  this.address = undefined;
+
+  next();
+});
+
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
